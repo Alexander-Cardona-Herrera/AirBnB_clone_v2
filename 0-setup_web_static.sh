48b8bbcf2ee1e4928ea8ba9651ebb04e4b54bc62
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-27 14:40:29 -0500

    adding task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,10 @@ then
 fi
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo 755 /data/web_static/releases/test/index.html
-echo "fake file with fake content" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+echo "fake file with fake content" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
 sudo chmod 755 /etc/nginx/sites-available/default
-sudo sed -i '/server_name _;/ a location /hbnb_static/ {\n\t alias /data/web_static/current/;' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/ a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
