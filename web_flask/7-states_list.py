306e093a061679d49c971e705c54412418ce8e4b
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-12 17:31:51 -0500

    adding task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,56 +6,23 @@
 
 
 from flask import Flask, escape, request, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def base_display():
-    """ Method for inicial display of tests """
-    return 'Hello HBNB!'
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """ Method that display a list of states """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display():
-    """ Method for display of tests """
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def display_text(text):
-    """ Method that display the text's pass in route """
-    return "C "+str(text).replace('_', ' ')
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_default_text(text='is cool'):
-    """ Method that display the text's pass in route,
-        default case='is cool' """
-    return "Python "+str(text).replace('_', ' ')
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    """ Method that display the number if an int is pass in route """
-    return str(n)+" is a number"
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_html(n):
-    """ Method that display 5-number.html if an int is pass in route """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def display_odd_html(n):
-    """ Method that display 6-number_odd_or_even.html if an int is pass """
-    if (n % 2) == 0:
-        n = "{} is even".format(n)
-    else:
-        n = "{} is odd".format(n)
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.teardown_appcontext
+def teardown(exception):
+    """ Method to close the current database session """
+    storage.close()
 
 
 if __name__ == '__main__':
