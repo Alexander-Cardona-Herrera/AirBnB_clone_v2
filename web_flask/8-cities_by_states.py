083923291c9488a46368eb120c108bf869e9a9c3
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-12 18:48:20 -0500

    adding pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,7 +19,8 @@ def display_states():
     """ Method that display a list of states """
     states = storage.all(State)
     cities = storage.all(City)
-    return render_template('8-cities_by_states.html', states=states, cities=cities)
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
 
 
 @app.teardown_appcontext
