f1f20fcb3bf506d8a5281c3f86e53dfddbaccc9f
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-12 16:53:21 -0500

    solving saving problems

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from os import getenv
@@ -15,8 +15,9 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
+            from models.city import City
             list_cities = []
-            for cities in all(City).values():
+            for cities in models.storage.all(City).values():
                 if (cities.state_id) == self.id:
                     list_cities.append(cities)
             return list_cities
